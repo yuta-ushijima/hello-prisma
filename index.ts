a082f3b76b618c12847eb34de9cc0313582dac05
Yuta Ushijima <register@yuta-u.com>
2022-08-04 23:33:11 +0900

    First Commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({ log: ['query']})
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  })
+  console.log(post)
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
